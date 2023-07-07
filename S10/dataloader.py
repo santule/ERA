9deb681f0f5bdeb1ff8bb85b5b34df3486fa4ff2
Sanjana Tule <sanjana.tule@gmail.com>
2023-07-07 23:46:24 +1000

    Add files via upload

---
+++ S10/dataloader.py
@@ -0,0 +1,77 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import model # model.py file
+import utils
+import dataloader
+import numpy as np
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+means = [0.4914, 0.4822, 0.4465]
+stds  = [0.2470, 0.2435, 0.2616]
+
+
+# Train Phase transformations
+train_transforms = A.Compose(
+    [
+        A.Normalize(mean=means, std=stds, always_apply=True),
+        A.PadIfNeeded(min_height=36, min_width=36, always_apply=True),
+        A.RandomCrop(height=32, width=32, always_apply=True),
+        A.HorizontalFlip(),
+        A.CoarseDropout(max_holes=1, max_height=8, max_width=8, min_holes=1, min_height=8, min_width=8, fill_value=means),
+        ToTensorV2(),
+    ]
+)
+
+test_transforms = A.Compose(
+    [
+        A.Normalize(mean=means, std=stds, always_apply=True),
+        ToTensorV2(),
+    ]
+)
+
+# dataset for CIFAR10 dataset
+class CIFAR10Dataset(datasets.CIFAR10):
+
+ def __init__(self,root="./data",train=True,download=True,transform=None):
+   super().__init__(root=root,train=train,download=download,transform=transform)
+
+ def __getitem__(self,index):
+   image, label = self.data[index], self.targets[index]
+
+   if self.transform is not None:
+     transformed = self.transform(image=image)
+     image = transformed["image"]
+   return image,label
+
+
+def load_dataset():
+    SEED = 1
+    cuda = torch.cuda.is_available()
+    print("CUDA Available?", cuda)
+
+    # For reproducibility
+    torch.manual_seed(SEED)
+
+    if cuda:
+     torch.cuda.manual_seed(SEED)
+
+    # dataloader arguments
+    dataloader_args = dict(shuffle=True, batch_size=128, num_workers=2, pin_memory=True) if cuda else dict(shuffle=True, batch_size=64)
+
+    # train dataloader
+    train = CIFAR10Dataset(transform = train_transforms)
+    train_loader = torch.utils.data.DataLoader(train, **dataloader_args)
+
+    # test dataloader
+    test = CIFAR10Dataset(transform = test_transforms,train=False)
+    test_loader = torch.utils.data.DataLoader(test, **dataloader_args)
+
+    return train_loader,test_loader
