ba880f17f8a8d4eee796f07dad0515db1782958e
Sanjana Tule <sanjana.tule@gmail.com>
2023-07-28 21:48:59 +1000

    Add files via upload

--- S11/utils.py
+++ S11/utils.py
@@ -114,7 +114,7 @@ def train(model, device, train_loader, optimizer, epoch,criterion,scheduler):
   train_acc = 100 * correct / processed
 
   print('\nTraining set set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-        train_loss, correct, len(train_loader),train_acc))
+        train_loss, correct, len(train_loader.dataset),train_acc))
 
   return train_loss, train_acc
 
@@ -133,9 +133,9 @@ def test(model, device, test_loader,criterion):
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-    test_acc = 100 * correct / len(test_loader)
+    test_acc = 100 * correct / len(test_loader.dataset)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(test_loss, correct, len(test_loader),test_acc))
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(test_loss, correct, len(test_loader.dataset),test_acc))
 
     return test_loss, test_acc
 
