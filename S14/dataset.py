935f3e53690011b89393b1670da530e4f6046dde
Sanjana Tule <sanjana.tule@gmail.com>
2023-09-02 10:24:27 +1000

    Add files via upload

--- S14/dataset.py
+++ S14/dataset.py
@@ -1,98 +1,89 @@
 import torch
-from torch.utils.data import Dataset,random_split, DataLoader
-from datasets import load_dataset
-from tokenizers import Tokenizer
-from tokenizers.models import WordLevel
-from tokenizers.trainers import WordLevelTrainer
-from tokenizers.pre_tokenizers import Whitespace
+import torch.nn as nn
+from torch.utils.data import Dataset
 
 
-# dataset for input data
 class BilingualDataset(Dataset):
+    def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len):
+        super().__init__()
+        self.seq_len = seq_len
+        self.ds = ds
+        self.tokenizer_src = tokenizer_src
+        self.tokenizer_tgt = tokenizer_tgt
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
 
-  def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len):
+        self.sos_token = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
+        self.eos_token = torch.tensor([tokenizer_tgt.token_to_id("[EOS]")], dtype=torch.int64)
+        self.pad_token = torch.tensor([tokenizer_tgt.token_to_id("[PAD]")], dtype=torch.int64)
 
-    super().__init__()
-    self.seq_len = seq_len
+    def __len__(self):
+        return(len(self.ds))
+    
+    def __getitem__(self, idx):
+        src_target_pair = self.ds[idx]
+        src_text = src_target_pair['translation'][self.src_lang]
+        tgt_text = src_target_pair['translation'][self.tgt_lang]
 
-    self.ds = ds
-    self.tokenizer_src = tokenizer_src
-    self.tokenizer_tgt = tokenizer_tgt
-    self.src_lang = src_lang
-    self.tgt_lang = tgt_lang
+        # Transform the text into tokens
+        enc_input_tokens = self.tokenizer_src.encode(src_text).ids
+        dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
 
-    self.sos_token = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype = torch.int64)
-    self.eos_token = torch.tensor([tokenizer_tgt.token_to_id("[EOS]")], dtype = torch.int64)
-    self.pad_token = torch.tensor([tokenizer_tgt.token_to_id("[PAD]")], dtype = torch.int64)
-    print(self.sos_token,self.eos_token,self.pad_token)
+        # Add sos, eos, padding to each sentence
+        enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2 # we will add <s> and </s>
+        # We will only add <s>, and </s> only on the label
+        dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1
 
+        # Make sure the number of padding tokens is not negative. If it is, the sentence is too long
+        if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
+            raise ValueError("Sentence is too long")
+        
+        # Add <s> and </s> token
+        encoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(enc_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64)
+            ],
+            dim=0
+        )
 
-  def __len__(self):
-    return len(self.ds)
+        # Add only </s> token
+        label = torch.cat(
+            [
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                self.eos_token,
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ],
+            dim=0
+        )
 
-  def __getitem__(self,idx):
-    src_target_pair = self.ds[idx]
-    src_text = src_target_pair['translation'][self.src_lang]
-    tgt_text = src_target_pair['translation'][self.tgt_lang]
+        # Add only <s> token
+        decoder_input = torch.cat(
+            [
+                self.sos_token,
+                torch.tensor(dec_input_tokens, dtype=torch.int64),
+                torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype=torch.int64)
+            ],
+            dim=0
+        )
 
-    # ecnode/ decoder tokens
-    enc_input_tokens = self.tokenizer_src.encode(src_text).ids
-    dec_input_tokens = self.tokenizer_tgt.encode(tgt_text).ids
+        # Double check the size of the tensors to make sure they are all seq_len long
+        assert encoder_input.size(0) == self.seq_len
+        assert decoder_input.size(0) == self.seq_len
+        assert label.size(0) == self.seq_len
 
-
-    # padding tokens
-    enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2 # EOS, SOS Tokens
-    dec_num_padding_tokens = self.seq_len - len(dec_input_tokens) - 1 # SOS only
-
-
-    if enc_num_padding_tokens < 0 or dec_num_padding_tokens < 0:
-      raise ValueError(" sentence is too long")
-
-    encoder_input = torch.cat(
-        [
-            self.sos_token,
-            torch.tensor(enc_input_tokens, dtype= torch.int64),
-            self.eos_token,
-            torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype = torch.int64)
-        ],
-        dim = 0
-    )
-
-    decoder_input = torch.cat(
-      [
-        self.sos_token,
-        torch.tensor(dec_input_tokens, dtype=torch.int64),
-        torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype = torch.int64)
-
-      ],
-      dim = 0
-    )
-
-    label = torch.cat(
-        [
-          torch.tensor(dec_input_tokens, dtype=torch.int64),
-          self.eos_token,
-          torch.tensor([self.pad_token] * dec_num_padding_tokens, dtype = torch.int64)
-        ]
-    )
-
-    assert encoder_input.size(0) == self.seq_len
-    assert decoder_input.size(0) == self.seq_len
-    assert label.size(0) == self.seq_len
-
-    return {
-        "encoder_input": encoder_input,
-        "decoder_input": decoder_input,
-        "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(),
-
-        "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int() & casual_mask(decoder_input.size(0)),
-        "label":label,
-        "src_text": src_text,
-        "tgt_text": tgt_text,
-        "raw_decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(),
-        "casual_mask":casual_mask(decoder_input.size(0))
-    }
-
+        return({
+            "encoder_input": encoder_input, # (seq_len)
+            "decoder_input": decoder_input, # (seq_len)
+            "encoder_mask": (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int(), # (1, 1, seq_len)
+            "decoder_mask": (decoder_input != self.pad_token).unsqueeze(0).int() & casual_mask(decoder_input.size(0)), # (1, seq_len)
+            "label": label, # (seq_len)
+            "src_text": src_text,
+            "tgt_text": tgt_text
+        })
+    
 def casual_mask(size):
-  mask = torch.triu(torch.ones((1,size,size)),diagonal=1).type(torch.int)
-  return mask == 0
+    mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+    return(mask == 0)
---
+++ S14/main_train.py
@@ -0,0 +1,187 @@
+import lightning.pytorch as pl
+from lightning.pytorch.callbacks import ModelCheckpoint
+from lightning.pytorch.loggers import TensorBoardLogger
+import torch
+import torch.nn as nn
+import torchmetrics
+import warnings
+warnings.filterwarnings("ignore")
+import logging
+from model import build_transformer
+from config import get_config
+from train import get_ds
+
+logger = logging.getLogger("Transformer")
+logger.setLevel(level=logging.INFO)
+fileHandler = logging.FileHandler(filename='prediction.log')
+fileHandler.setLevel(level=logging.INFO)
+logger.addHandler(fileHandler)
+
+class LitTr(pl.LightningModule):
+    def __init__(self,cfg,tokenizer_src,tokenizer_tgt):
+        super().__init__()
+        
+        vocab_src_len = tokenizer_src.get_vocab_size()
+        vocab_tgt_len = tokenizer_tgt.get_vocab_size()
+        self.model = build_transformer(vocab_src_len, vocab_tgt_len, cfg['seq_len'], cfg['seq_len'], d_model=cfg['d_model'])
+        self.loss_fn = nn.CrossEntropyLoss(ignore_index=tokenizer_src.token_to_id('[PAD]'), label_smoothing=0.1)
+        self.BATCH_SIZE = cfg['batch_size']
+        self.num_epochs = cfg['num_epochs']
+        self.lr = cfg['lr']
+        self.tokenizer_src = tokenizer_src
+        self.tokenizer_tgt = tokenizer_tgt
+        self.seq_len = cfg['seq_len']
+        self.expected = []
+        self.predicted = []
+        self.initial_epoch = 0
+        self.save_hyperparameters()
+        self.train_loss = []
+
+
+    def forward(self, x):
+
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+
+        encoder_input = batch['encoder_input']
+        decoder_input = batch['decoder_input']
+        encoder_mask  = batch['encoder_mask']
+        decoder_mask  = batch['decoder_mask']
+
+        # Run the tensors through the encoder, decoder and the projection layer
+        encoder_output = self.model.encode(encoder_input, encoder_mask) # (b, seq_len, d_model)
+        decoder_output = self.model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask)
+        proj_output = self.model.project(decoder_output) # ( b, seq_len, vocab_size)
+
+        label = batch['label']
+
+        loss = self.loss_fn(proj_output.view(-1, self.tokenizer_tgt.get_vocab_size()), label.view(-1))
+        self.train_loss.append(loss)
+
+        self.log('train_loss', loss, prog_bar=True, on_step=False, on_epoch=True,logger=True)
+        return loss
+
+    def casual_mask(self, size):
+        mask = torch.triu(torch.ones((1, size, size)), diagonal=1).type(torch.int)
+        return(mask == 0)
+
+    def greedy_decode(self,source, source_mask):
+
+        sos_idx = self.tokenizer_tgt.token_to_id('[SOS]')
+        eos_idx = self.tokenizer_tgt.token_to_id('[EOS]')
+
+        encoder_output = self.model.encode(source, source_mask)
+        decoder_input = torch.empty(1,1).fill_(sos_idx).type_as(source)
+
+        while True:
+            if decoder_input.size(1) == self.seq_len:
+              break
+
+            decoder_mask = self.casual_mask(decoder_input.size(1)).type_as(source_mask)
+
+            out = self.model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
+
+            prob = self.model.project(out[:,-1])
+            _,next_word = torch.max(prob, dim=1)
+
+            decoder_input = torch.cat(
+                [decoder_input, torch.empty(1,1).type_as(source).fill_(next_word.item())], dim=1
+            )
+
+            if next_word == eos_idx:
+              break
+
+        return decoder_input.squeeze(0)
+
+    def on_train_epoch_end(self):
+
+        self.log('loss', torch.stack(self.train_loss).mean(), on_epoch=True, logger=True)
+        print(f"Loss Mean - {torch.stack(self.train_loss).mean()}")
+        self.train_loss.clear()
+
+    def evaluate(self, batch, stage=None):
+
+        encoder_input = batch["encoder_input"]
+        encoder_mask = batch["encoder_mask"]
+
+       
+        model_out = self.greedy_decode(encoder_input, encoder_mask)
+
+        source_text = batch["src_text"][0]
+        target_text = batch["tgt_text"][0]
+
+
+        model_out_text = self.tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+        logger.info(f"SOURCE - {source_text}")
+        logger.info(f"TARGET - {target_text}")
+        logger.info(f"PREDICTED - {model_out_text}")
+        logger.info("=============================================================")
+
+        self.expected.append(target_text)
+        self.predicted.append(model_out_text)
+
+
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "val")
+
+    def on_validation_epoch_end(self):
+        metric = torchmetrics.CharErrorRate()
+        cer = metric(self.predicted, self.expected)
+        self.log('validation_cer', cer, prog_bar=True, on_epoch=True, logger=True)
+
+
+        # Compute the word error rate
+        metric = torchmetrics.WordErrorRate()
+        wer = metric(self.predicted, self.expected)
+        self.log('validation_wer', wer, prog_bar=True, on_epoch=True, logger=True)
+
+        # Compute the BLEU metric
+        metric = torchmetrics.BLEUScore()
+        bleu = metric(self.predicted, self.expected)
+        self.log('validation_bleu', bleu, prog_bar=True, on_epoch=True, logger=True)
+
+        self.expected.clear()
+        self.predicted.clear()
+
+    def optimizer_zero_grad(self, epoch, batch_idx, optimizer):
+        optimizer.zero_grad(set_to_none=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr = self.lr, eps = 1e-9)
+
+        return optimizer
+
+
+trainer = pl.Trainer(log_every_n_steps=1,
+                     limit_val_batches=2,
+                     check_val_every_n_epoch=1,
+                     max_epochs=10, 
+                     accelerator='auto',
+                     devices='auto',
+                     strategy='auto',
+                     logger=[TensorBoardLogger("logs/", name="transformer-bilin")],
+    )
+
+def main():
+
+    # config
+    cfg = get_config()
+    cfg['preload']=None
+    cfg['num_epochs'] = 10
+    cfg['batch_size'] = 16
+
+    #### Loading Datasets
+    train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt = get_ds(cfg)
+
+    #### Loading the lightning module
+    model = LitTr(cfg,tokenizer_src,tokenizer_tgt)
+    trainer.fit(model, train_dataloader,val_dataloader)
+
+
+if __name__ == "__main__":
+  main()
