627b1f03abb64fe789625b11448f2479de89366b
Sanjana Tule <sanjana.tule@gmail.com>
2024-01-30 14:29:18 +1000

    Add files via upload

--- capstone_part2/step2_finetune/step2_dataset.py
+++ capstone_part2/step2_finetune/step2_dataset.py
@@ -12,9 +12,8 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
 class llavadataset(Dataset):
-  def __init__(self, qa_dataset, phi_model_name, clip_model_name, tokenizer,processor):
+  def __init__(self, qa_dataset, phi_model_name, clip_model_name,processor):
 
-    self.tokenizer  = tokenizer
     self.processor  = processor
     self.qa_dataset = qa_dataset
 
@@ -31,14 +30,12 @@ class llavadataset(Dataset):
     image_load = Image.open(requests.get(img_url,stream=True).raw)
     image_processed = self.processor(images=image_load, return_tensors="pt") ['pixel_values']
     image_processed = image_processed.squeeze(0)
-    #q = self.tokenizer(ques, return_tensors="pt", return_attention_mask=False)['input_ids'].squeeze(0)
-    #a = self.tokenizer(ans, return_tensors="pt", return_attention_mask=False)['input_ids'].squeeze(0)
     return(image_processed , ques, ans)
   
 
 def collate_fn(batch):
     image_embeddings, ques,ans = zip(*batch)
     image_embeddings_stacked = torch.stack(image_embeddings, dim=0)
-    ques_padded = torch.nn.utils.rnn.pad_sequence(ques, batch_first=True, padding_value=50256)
-    ans_padded = torch.nn.utils.rnn.pad_sequence(ans, batch_first=True, padding_value=50256)
+    ques_padded = torch.nn.utils.rnn.pad_sequence(ques, batch_first=True, padding_value=50296)
+    ans_padded = torch.nn.utils.rnn.pad_sequence(ans, batch_first=True, padding_value=50296)
     return (image_embeddings_stacked, ques_padded,ans_padded)
